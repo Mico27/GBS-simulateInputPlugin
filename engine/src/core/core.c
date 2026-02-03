d888459088a868065a1824476ce26a3c2ccbafc6
Mico27 <mickaelfortier616@msn.com>
2026-02-03 15:41:21 -0500

    2.4.0 Beta3 Update

--- engine/src/core/core.c
+++ engine/src/core/core.c
@@ -87,7 +87,10 @@ void process_VM(void) {
                 scroll_update();
                 actors_update();
                 actors_render();
-                projectiles_update();                                   // update and render projectiles
+                if (projectiles_active_head) {
+                    projectiles_update();                               // update projectiles
+                    projectiles_render();                               // render projectiles
+                }
 
                 ui_update();
                 actors_handle_player_collision();
